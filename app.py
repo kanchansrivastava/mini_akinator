63d6d3b4da966173afdac628f50d479915e994eb
Kanchan Srivastava <kanchan.srivastava@cdk.com>
2017-01-20 13:20:45 +0530

    added dynamic behavior in the routes and views v0.2

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
 from flask import Flask
 
+
 app = Flask(__name__)
+guesses = ['Python', 'Java', 'C++']
+questions = ['Is it compiled?', 'Does it run on a VM?', 'Does it enforce indentation?']
 
 
 @app.route('/')
 def index():
-    return 'Hello Flask!'
+    return 'Welcome to Mini Akinator!!'
+
+
+@app.route('/guess/<int:id>')
+def guess(id):
+    return 'My guess: {0}'.format(guesses[id])
+
+
+@app.route('/question/<int:id>')
+def question(id):
+    return '{0} What is correct answer? [Yes/No]'.format(questions[id])
 
 
 if __name__ == '__main__':
-    app.run()
-    # app.run(debug=True)
+    app.run(debug=True)
+    # app.run()
     # app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+
+
+
+
