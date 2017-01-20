15de3da68e3d862a7d60c29db287424d1a73fb0a
Kanchan Srivastava <kanchan.srivastava@cdk.com>
2017-01-20 15:07:37 +0530

    implementing game logic v0.9.1

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_wtf import Form
-from wtforms.fields import RadioField, SubmitField
+from wtforms.fields import RadioField, SubmitField, StringField
+
+from mini_akinator import Guess
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'shhhh!!!secret'
-guesses = ['Python', 'Java', 'C++']
-questions = ['Is it compiled?', 'Does it run on a VM?', 'Does it enforce indentation?']
+
+game = Guess('Python')
+game.expand('Python', 'C++', 'Is it interpreted?', False)
+game.expand('C++', 'Java', 'Does it run on a VM?', True)
 
 
 class YesNoQuestionForm(Form):
     answer = RadioField('Your answer', choices=[('yes', 'Yes'), ('no', 'No')])
+    submit = SubmitField('Submit')
+
+
+class LearnForm(Form):
+    language = StringField('What language did you pick?')
+    question = StringField('What is a question that differentiates your '
+                           'language from mine?')
+    answer = RadioField('What is the answer for your language?',
+                        choices=[('yes', 'Yes'), ('no', 'No')])
     submit = SubmitField('Submit')
 
 
@@ -19,20 +32,37 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/guess/<int:id>')
+@app.route('/guess/<int:id>', methods=['GET', 'POST'])
 def guess(id):
-    return render_template('guess.html', guess=guesses[id])
+    form = YesNoQuestionForm()
+    if form.validate_on_submit():
+        if form.answer.data == 'yes':
+            return redirect(url_for('index'))
+        return redirect(url_for('learn', id=id))
+    return render_template('guess.html', guess=game.get_guess(id), form=form)
 
 
 @app.route('/question/<int:id>', methods=['GET', 'POST'])
 def question(id):
+    question = game.get_question(id)
+    if question is None:
+        return redirect(url_for('guess', id=id))
     form = YesNoQuestionForm()
     if form.validate_on_submit():
-        if form.answer.data == 'yes':
-            return redirect(url_for('question', id=id+1))
-        else:
-            return redirect(url_for('question', id=id))
-    return render_template('question.html', question=questions[id], question_number=id, form=form)
+        new_id = game.answer_question(form.answer.data == 'yes', id)
+        return redirect(url_for('question', id=new_id))
+    return render_template('question.html', question=question, question_number=id, form=form)
+
+
+@app.route('/learn/<int:id>', methods=['GET', 'POST'])
+def learn(id):
+    guess = game.get_guess(id)
+    form = LearnForm()
+    if form.validate_on_submit():
+        game.expand(guess, form.language.data, form.question.data,
+                    form.answer.data == 'yes')
+        return redirect(url_for('index'))
+    return render_template('learn.html', guess=guess, form=form)
 
 
 if __name__ == '__main__':
