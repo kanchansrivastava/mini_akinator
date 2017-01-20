cf71953413030d35bd45ceff1bb11783da127d5a
Kanchan Srivastava <kanchan.srivastava@cdk.com>
2017-01-20 13:18:33 +0530

    Basic-view and routes and setting up server to run v0.1

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello Flask!'
+
+
+if __name__ == '__main__':
+    app.run()
+    # app.run(debug=True)
+    # app.run(host='0.0.0.0', port=5000, debug=True)
